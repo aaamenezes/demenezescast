5bfefbadfbd1e3187764b43dd57580919c50fe1b
André Menezes <1992menezes@gmail.com>
2022-11-09 23:25:46 -0300

    Change darkMode component

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,54 +1,73 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { ColorModeContext } from './ColorMode'
 
-const StyledDarkMode = styled.label`
+const StyledSwitch = styled.div`
+  background-color: #333333;
+  border: 0;
+  padding: 3px;
+  font-size: 12px;
+  width: 50px;
+  height: 25px;
   display: flex;
-  align-items: center;
-  padding: 0.5rem;
-  height: 40px;
-  border: 1px solid ${ ({ theme }) => theme.border };
-  border-radius: 4px;
-  cursor: pointer;
+  justify-content: space-around;
+  border-radius: 10000px;
+  position: relative;
+  border: 1px solid ${ ({ theme }) => theme.background };
 
-  * {
-    cursor: pointer;
-  }
-
-  .input {
-    display: none;
-    margin-right: 0.5rem;
-  }
-
-  i {
-    margin-right: 0.5rem;
+  label {
+    width: 50px;
   }
 
   span {
-    letter-spacing: 1px;
-    font-size: 0.8rem;
-    font-weight: 700;
+    display: inline-flex;
+    width: 20px;
+    height: 20px;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  label::before {
+    content: "";
+    background-color: #fafafa;
+    border: 1px solid #333333;
+    width: 21px;
+    height: 21px;
+    border-radius: 100%;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    transition: .3s;
+    cursor: pointer;
+  }
+  
+  input[type="checkbox"] {
+    display: none;
+
+    &:checked + label::before {
+      transform: translateX(110%);
+    }
   }
 `
 
-export default function Mode() {
-  const { mode, handleMode } = useContext(ColorModeContext)
-
-  const iconClassName = 'fa-regular fa-'.concat(
-    mode === 'light' ? 'sun' : 'moon'
-  )
+export default function DarkModeSwitch() {
+  const { handleMode } = React.useContext(ColorModeContext)
 
   return (
-    <StyledDarkMode>
+    <StyledSwitch>
       <input
+        id='darkmode'
         type='checkbox'
-        checked={mode === 'dark'}
         onChange={handleMode}
-        className='input'
       />
-      <i className={iconClassName} />
-      <span>{mode.toUpperCase()}</span>
-    </StyledDarkMode>
-
+      <label
+        htmlFor='darkmode'
+        className='darkmode-switch'
+      >
+        <span>🌙</span>
+        <span>☀️</span>
+      </label>
+    </StyledSwitch>
   )
 }
